d5f29cda2bbd5f72f27a97b2f52552cc847aecac
Jhonny Afonso <jafonso@teravisiontech.com>
2022-06-02 17:06:04 -0500

    Section 03 - Class 30

---
+++ 03/src/index.js
@@ -0,0 +1,39 @@
+// Import the React and ReactDom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { createRoot } from 'react-dom/client';
+
+// Create a React Component
+const App =  function () {
+    return (
+      <div className="ui container comments">
+          <div className="comment">
+              <a href="/" className="avatar">
+                  <img alt="avatar" />
+              </a>
+              <div className="content">
+                  <a href="/" className="author">
+                      Sam
+                  </a>
+                  <div className="metadata">
+                      <span className="date">Today at 6:00PM</span>
+                  </div>
+                  <div className="text">
+                      Nice blog post!
+                  </div>
+              </div>
+          </div>
+      </div>  
+    );
+};
+
+// Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
+// React 18
+//const container = document.querySelector('#root')
+//const root = createRoot(container);
+//root.render(<App />);
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ https://www.udemy.com/course/react-redux
 
 ## Section 01 - Let's Dive In!
 
+## Section 02 - Building Content with JSX
 
+## Section 03 - Communicating with Props
