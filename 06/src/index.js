6774273f7626919fef809ca9287fdea50f0c0c58
Jhonny Afonso <jafonso@teravisiontech.com>
2022-06-07 17:08:00 -0500

    Section 06 - Class 75

--- 06/src/index.js
+++ 06/src/index.js
@@ -15,17 +15,25 @@ class App extends React.Component {
             err => this.setState({ errorMessage: err.message })
         );
     }
-
-    render () {
+    
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
-        
+
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />;
         }
-        
-        return <Spinner />;
+
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render () {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
