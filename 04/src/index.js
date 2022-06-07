87a620e0e690bb931d0d2547065c5e73fc6ed4e8
Jhonny Afonso <jafonso@teravisiontech.com>
2022-06-07 12:27:26 -0500

    Section 04 - Class 52

--- 04/src/index.js
+++ 04/src/index.js
@@ -6,6 +6,10 @@ import ReactDOM from 'react-dom';
 
 // Create a React Component
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
     return (
         <div>Hi There!</div>
     );
